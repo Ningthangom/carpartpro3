59e3a73f6ed6350639161946d8f6c877e7dab2fb
Ningthangom <ningthangom@gmail.com>
2021-02-04 16:06:01 +1030

    fixed typepo in server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use(require('./routes/user'))
 
 if(process.env.NODE_ENV=="production"){
     app.use(express.static('client/build'))
-     const path = required('path')
+     const path = require ('path')
     app.get("*",(req,res)=> {
         res.sendFile(path.join(__dirname,'client','build','index.html'))
     }) 
